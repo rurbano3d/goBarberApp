0d54833aef43f43a4fbec2b5868cf4754823cb99
rurbano3d <rurbano3d@gmail.com>
2019-10-29 14:40:21 -0300

    Pages and components

--- src/Pages/SignIn/index.js
+++ src/Pages/SignIn/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text } from 'react-native';
 
 import Background from '~/components/Background';
+import Input from '~/components/Input';
+import Button from '~/components/Button';
 // import { Container } from './styles';
 
 export default function SignIn() {
   return (
     <Background>
       <Text>SignIn</Text>
+      <Input icon="call" placeholder="Digite seu nome" />
+      <Button>Entrar</Button>
     </Background>
   );
 }
