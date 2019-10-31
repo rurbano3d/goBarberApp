75f1cae39fb9eec9d386645726b8cad507014ef7
rurbano3d <rurbano3d@gmail.com>
2019-10-30 22:25:18 -0300

    inside routes

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,40 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+
 import SignIn from './Pages/SignIn';
 import SignUp from './Pages/SignUp';
 
-export default createAppContainer(
-  createSwitchNavigator({
-    SignIn,
-    SignUp,
-  })
-);
+import Dashboard from './Pages/Dashboard';
+import Profile from './Pages/Profile';
+
+export default (isSigned = false) =>
+  createAppContainer(
+    createSwitchNavigator(
+      {
+        Sign: createSwitchNavigator({
+          SignIn,
+          SignUp,
+        }),
+        App: createBottomTabNavigator(
+          {
+            Dashboard,
+            Profile,
+          },
+          {
+            tabBarOptions: {
+              keyboardHidesTabBar: true,
+              activeTintColor: '#fff',
+              inactiveTintColor: 'rgba(255,255,255,0.6)',
+              style: {
+                backgroundColor: '#8d41a8',
+              },
+            },
+          }
+        ),
+      },
+      {
+        initialRouteName: isSigned ? 'App' : 'Sign',
+      }
+    )
+  );
