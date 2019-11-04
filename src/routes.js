a2a08946f64befa4a4277155d6938422cc86575a
rurbano3d <rurbano3d@gmail.com>
2019-11-04 10:59:29 -0300

    Page Select Provider done

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,17 @@
+import React from 'react';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import SignIn from './Pages/SignIn';
 import SignUp from './Pages/SignUp';
+
+import SelectProvider from './Pages/New/SelectProvider';
+import SelectDateTime from './Pages/New/SelectDateTime';
+import Confirm from './Pages/New/Confirm';
 
 import Dashboard from './Pages/Dashboard';
 import Profile from './Pages/Profile';
@@ -19,6 +27,35 @@ export default (isSigned = false) =>
         App: createBottomTabNavigator(
           {
             Dashboard,
+            New: {
+              screen: createStackNavigator(
+                {
+                  SelectProvider,
+                  SelectDateTime,
+                  Confirm,
+                },
+                {
+                  defaultNavigationOptions: {
+                    headerTransparent: true,
+                    headerTintColor: '#FFF',
+                    headerLeftContainerStyle: {
+                      marginLeft: 20,
+                    },
+                  },
+                }
+              ),
+              navigationOptions: {
+                tabBarVisible: false,
+                tabBarLabel: 'Agendar',
+                tabBarIcon: (
+                  <Icon
+                    name="add-circle-outline"
+                    size={20}
+                    color="rgba(255,255,255,0.6)"
+                  />
+                ),
+              },
+            },
             Profile,
           },
           {
